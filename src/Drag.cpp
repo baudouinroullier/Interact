8336ff5b30d7c19c87e3fea5009878bfe04c3b89
Baudouin Roullier <baudouin.roullier@exotec.com>
2024-09-18 09:48:00 +0200

    change return of processEvent to notify when dragged

--- src/Drag.cpp
+++ src/Drag.cpp
@@ -14,27 +14,35 @@ void Drag::setStateChangeCallback(std::function<void(sf::Shape&, bool)> callback
 
 bool Drag::processEvent(sf::Event event, sf::Shape& shape)
 {
+    bool caught = false;
     bool oldActive = m_active;
 
     if (!m_active &&
-        event.type == sf::Event::EventType::MouseButtonPressed &&
+        event.type == sf::Event::MouseButtonPressed &&
         event.mouseButton.button == sf::Mouse::Button::Left &&
         shape.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
     {
         m_active = true;
         m_relClickPos = shape.getPosition() - sf::Vector2f{event.mouseButton.x, event.mouseButton.y};
+        caught = true;
     }
     else if (m_active)
     {
-        if (event.type == sf::Event::EventType::MouseButtonReleased)
+        if (event.type == sf::Event::MouseButtonReleased)
+        {
             m_active = false;
+            caught = true;
+        }
         else if (event.type == sf::Event::MouseMoved)
+        {
             shape.setPosition(m_relClickPos + sf::Vector2f{event.mouseMove.x, event.mouseMove.y});
+            caught = true;
+        }
     }
 
     m_stateChangeCallback(shape, m_active);
 
-    return oldActive != m_active;
+    return caught;
 }
 
 } // namespace act
